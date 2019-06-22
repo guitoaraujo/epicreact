bd7130f306c75e05a9bd7c00edda6fa58026974d
Guito Araujo <guitoaraujo@gmail.com>
2019-06-22 12:20:01 -0300

    hummm...

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log('HAHAHAhaha')
+
+let a;
+
+a = 123
+b = 123
+c = a+b
+
+console.log(c)
+console.log(c+5)
+console.log(c +5)
+console.log(c + 5)
+console.log(0 == false)
+console.log(0 == undefined)
+console.log(0 == null)
+console.log(3 == '3')
+console.log(3 === '3')
