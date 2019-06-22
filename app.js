7ca37b1cc306e851b5e66ab93ad634abab7d9f55
Guito Araujo <guitoaraujo@gmail.com>
2019-06-22 14:16:57 -0300

    teste

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-console.log('HAHAHAhaha')
+const axios  = require('axios')
 
-let a;
-
-a = 123
-b = 123
-c = a+b
-
-console.log(c)
-console.log(c+5)
-console.log(c +5)
-console.log(c + 5)
-console.log(0 == false)
-console.log(0 == undefined)
-console.log(0 == null)
-console.log(3 == '3')
-console.log(3 === '3')
+axios.get('https://swapi.co/api/films')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
